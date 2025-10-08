8f366b8b402185c985c4ed688cf70277f308d2d7
SERGIO NP <serganoh@uacam.mx>
2025-10-08 03:05:10 -0600

    http - basic http server added

---
+++ serversdemo/src/main/java/servers/tests/App.java
@@ -0,0 +1,12 @@
+package servers.tests;
+
+import java.util.concurrent.Executors
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var service = Executors.newSingleThreadExecutor();
+        service
+    }
+}
